85efd526a860a73e71e4b74a88cfe6b1cd3deae0
yhy <2420267525@qq.com>
2023-12-02 15:07:28 +0800

    finish PureMF

--- code/model.py
+++ code/model.py
@@ -1,4 +1,6 @@
 from torch import nn
+import torch
+from dataloader import BasicDataset
 
 
 class BasicModel(nn.Module):
@@ -10,7 +12,55 @@ class BasicModel(nn.Module):
 
 
 class PureMF(BasicModel):
-    pass
+    """
+    矩阵分解模型
+    """
+    def __init__(self, config: dict, dataset: BasicDataset):
+        super(PureMF, self).__init__()
+        self.num_users = dataset.n_users
+        self.num_items = dataset.m_items
+        self.latent_dim = config['latent_dim_rec']
+        self.f = nn.Sigmoid
+        self.__init_weight()
+
+    def __init_weight(self):
+        self.embedding_user = nn.Embedding(self.num_users, self.latent_dim)
+        self.embedding_item = nn.Embedding(self.num_items, self.latent_dim)
+        print("初始化PureMF，使用标准分布，均值为0方差为1")
+
+    def getUsersRating(self, users):
+        users = users.long()
+        users_emb = self.embedding_user(users)
+        items_emb = self.embedding_item.weight
+        scores = torch.matmul(users_emb, items_emb.t())
+        return self.f(scores)
+
+    def forward(self, users, items):
+        # 想知道这里users和items的形状是什么啊？
+        users = users.long()
+        items = items.long()
+        users_emb = self.embedding_user(users)
+        items_emb = self.embedding_item(items)
+        # 每个元素相乘然后相加得到分数的总合
+        scores = torch.sum(users_emb * items_emb, dim=1)
+        return self.f(scores)
+
+    def bpr_loss(self, users, pos, neg):
+        users_emb = self.embedding_user(users.long())
+        pos_emb = self.embedding_item(pos.long())
+        neg_emb = self.embedding_item(neg.long())
+        pos_scores = torch.sum(users_emb * pos_emb, dim=1)
+        neg_scores = torch.sum(users_emb * neg_emb, dim=1)
+        # softplus(x) = log(1 + exp(x))是一个激活函数
+        loss = torch.mean(nn.functional.softplus(neg_scores - pos_scores))
+        # 这是L2正则化
+        reg_loss = ((1 / 2) * (users_emb.norm(2).pow(2) + pos_emb.norm(2).pow(2) + neg_emb.norm(2).pow(2)) /
+                    float(len(users)))
+        return loss, reg_loss
+
+
+
+
 
 class LightGCN(BasicModel):
     pass
--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 # This is a sample Python script.
 import numpy as np
 from scipy.sparse import csr_matrix
+import torch
+import torch.nn as nn
 
 if __name__ == '__main__':
-    row = np.array([0, 0, 1, 2, 2, 2])
-    col = np.array([0, 2, 2, 0, 1, 3])
-    data = np.array(np.ones(6))
-    sparse_m = csr_matrix((data, (row, col)), shape=(5, 7)).toarray()
-    print(sparse_m, 'shape = ', sparse_m.shape)
-    #
-    # users_D = np.array(sparse_m.sum(axis=1)).squeeze()
-    # items_D = np.array(sparse_m.sum(axis=0)).squeeze()
-    # print('users_D = ', users_D, 'shape = ', users_D.shape)
-    # print('items_D = ', items_D,  'shape = ', items_D.shape)
-    # print('=' * 30)
-    # users_D[users_D == 0.] = 1
-    # print('users_D = ', users_D)
-    # items_D[items_D == 0.] = 1
-    # print('items_D = ', items_D)
-    print(sparse_m.nonzero())
-    print(sparse_m.nonzero()[1])
+    print("=========以下是nn.embedding函数的test===========")
+    a = np.random.randint(0, 10, 8)
+    matrix = torch.from_numpy(a)
+    print(matrix)
+    print(matrix.size())
+    emb = nn.Embedding(10, 5)
+    # print(emb.weight)
+    # print(emb.sparse)
+    m_emb = emb(matrix)
+    print('m_emb = ', m_emb)
+    print(m_emb.size())
+    print(type(m_emb))
+    print(m_emb.norm(2))
+
+
+
+    print("=========以下是csr_matrix函数的test===========")
+    # row = np.array([0, 0, 1, 2, 2, 2])
+    # col = np.array([0, 2, 2, 0, 1, 3])
+    # data = np.array(np.ones(6))
+    # sparse_m = csr_matrix((data, (row, col)), shape=(5, 7)).toarray()
+    # print(sparse_m, 'shape = ', sparse_m.shape)
+    # #
+    # # users_D = np.array(sparse_m.sum(axis=1)).squeeze()
+    # # items_D = np.array(sparse_m.sum(axis=0)).squeeze()
+    # # print('users_D = ', users_D, 'shape = ', users_D.shape)
+    # # print('items_D = ', items_D,  'shape = ', items_D.shape)
+    # # print('=' * 30)
+    # # users_D[users_D == 0.] = 1
+    # # print('users_D = ', users_D)
+    # # items_D[items_D == 0.] = 1
+    # # print('items_D = ', items_D)
+    # print(sparse_m.nonzero())
+    # print(sparse_m.nonzero()[1])
 
