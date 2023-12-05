06a5088f5580852f1513d9440c3a6e3df9761dd6
yhy <2420267525@qq.com>
2023-12-05 15:56:36 +0800

    实现了构建稀疏图的函数

--- code/dataloader.py
+++ code/dataloader.py
@@ -1,4 +1,8 @@
+from time import time
+
 import numpy as np
+import scipy.sparse as sp
+import torch
 from scipy.sparse import csr_matrix
 from torch.utils.data import Dataset
 
@@ -53,7 +57,7 @@ class BasicDataset(Dataset):
         A =
             |I,   R|
             |R^T, I|
-        :return:
+        :return: 一个副对角线是relation矩阵的矩阵
         """
         raise NotImplementedError
 
@@ -206,7 +210,109 @@ class Loader(BasicDataset):
                 test_data[user] = [item]
         return test_data
 
+    def getSparseGraph(self):
+        """
 
+        :return: 返回一个用tensor表示的稀疏图
+        """
+        print("loading adjacency matrix")
+        if self.Graph is None:
+            try:
+                # npy是numpy的二进制格式，npz是其压缩文件，储存的是数组
+                pre_adj_mat = sp.load_npz(self.path + "/s_pre_adj_mat.npz")
+                print("successfully loaded...")
+                norm_adj = pre_adj_mat
+            except:
+                print("generating adjacency matrix")
+                start = time()
+                # Dictionary Of Keys based sparse matrix
+                # 创建一个(n + m) * (n + m)的矩阵
+                adj_mat = sp.dok_matrix((self.n_users + self.m_items, self.n_users + self.m_items), dtype=np.float32)
+                # Convert this matrix to List of Lists format.
+                adj_mat = adj_mat.tolil()
+                R = self.UserItemNet.tolil() # 这个是relation matrix
+                # 这里就是把副对角线变成R矩阵
+                adj_mat[:self.n_users, self.m_items:] = R
+                adj_mat[self.n_users:, :self.m_items] = R.T
+                # 再变回去
+                adj_mat = adj_mat.todok()
+                # adj_mat = adj_mat + sp.eye(adj_mat.shape[0]) 这里是加上了自连接但是代码上注释掉了
+
+                # 这个应该是计算adjacency matrix的度
+                # 把每一行的总合加起来，就是每一个节点的度，前n个是user，后面是item
+                rowsum = np.array(adj_mat.sum(axis=1))
+                # -0.5次方，然后展平
+                d_inv = np.power(rowsum, -0.5).flatten()
+                # 这一步感觉是防止有些节点的度是0，然后-0.5次方后就变成无限了，这里需要手动重新置0
+                d_inv[np.isinf(d_inv)] = 0.
+                # 这个d_mat 就是每个节点的degree matrix。是一个对角矩阵
+                d_mat = sp.diags(d_inv)
+
+                # A_hat = D -1/2 * A * D -1/2
+                norm_adj = d_mat.dot(adj_mat)
+                norm_adj = norm_adj.dot(d_mat)
+                # 到这一步成功构造出矩阵
+                norm_adj = norm_adj.tocsr()
+                end = time()
+                print(f"costing{end - start}s, save norm_mat...")
+                # 保存模型到对应dataset的文件夹里
+                sp.save_npz(self.path + "/s_pre_adj_mat.npz", norm_adj)
+
+                # 这个默认是false
+                if self.split:
+                    self.Graph = self._split_A_hat(norm_adj)
+                    print("done split A_hat")
+                else:
+                    self.Graph = self.convert_sp_mat_to_sp_tensor(norm_adj)
+                    # 在稀疏矩阵中，可能会出现同一个索引对应多个标量，coalesce是对相同索引的多个值求和
+                    # 这里我不知道为啥要用这个函数。。
+                    self.Graph = self.Graph.coalesce().to(world.devices)
+                    print("don't split the matrix...")
+
+        return self.Graph
+
+    def _split_A_hat(self, A_hat):
+        """
+        把A_hat矩阵根据config[a_fold]的设置按行分段
+        :param A_hat: symmetrically normalized matrix,A_hat = D -1/2 * A * D -1/2
+        :return: 返回分好段的列表
+        """
+        A_hat_fold = []
+        # 看下每一折有多长
+        # 默认是有100折
+        fold_len = (self.n_users + self.m_items) // self.folds
+        for i in range(self.folds):
+            start = i * fold_len
+            # 判断是不是最后一折
+            if i == self.folds - 1:
+                end = self.n_users + self.m_items
+            else:
+                end = start + fold_len
+
+            A_hat_fold.append(self.convert_sp_mat_to_sp_tensor(A_hat[start:end]).coalesce().to(world.devices))
+            return A_hat_fold
+
+    def convert_sp_mat_to_sp_tensor(self, sp_mat):
+        """
+        把传入的sp_mat变成tensor的形式
+        :param sp_mat: A_hat矩阵或者是A_hat的一部分
+        :return: 返回sp_mat的tensor形式
+        """
+        # 首先将矩阵转换成坐标的表示形式
+        coo = sp_mat.tocoo().astype(np.float32)
+        # 取到行列和对应的数据
+        row = torch.tensor(coo.row).long()
+        col = torch.tensor(coo.col).long()
+        data = torch.FloatTensor(col.data)
+        # 这个其实就是把row和col叠在一起
+        # index的shape为(2, len(data))
+        index = torch.stack([row, col])
+        # 返回的是一个矩阵的tensor表达形式
+        return torch.sparse.FloatTensor(indices=index, values=data, size=torch.Size(coo.shape))
+
+    def getUserItemFeedback(self, users, items):
+        # TODO
+        pass
 
 
 
